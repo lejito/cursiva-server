b04b209bc8c78ed479238888d278e17f59f4a462
Lejitop <alejocrrs@gmail.com>
2025-02-21 16:32:08 -0500

    build: Add dotenv configuration and enhance CORS options for improved security

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application } from "express";
 import cors from "cors";
 import router from "./routers/app.router";
+import "dotenv/config";
 
 const app: Application = express();
 
@@ -8,16 +9,20 @@ const whiteList = process.env.WHITELIST
 	? process.env.WHITELIST.split(",")
 	: ["*"];
 
+console.log("WhiteList: ", whiteList);
+
 const corsOptions = {
 	origin: function (origin: string | undefined, callback: Function) {
 		if (whiteList.includes("*")) return callback(null, true);
 		if (origin === undefined) return callback(null, true);
-		if (whiteList.indexOf(origin) !== -1) {
+		if (whiteList.indexOf(origin) !== -1 || !origin) {
 			callback(null, true);
 		} else {
 			callback(new Error("Not allowed by CORS"));
 		}
 	},
+	methods: ["GET", "POST", "PUT", "DELETE"],
+	optionsSuccessStatus: 200,
 };
 
 app.set("trust proxy", true);
